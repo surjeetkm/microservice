17e426383b8f18aef7a44eb289e63a3b4bff24f8
surjeetmohanty <surjeetmohanty2019@gmail.com>
2019-11-16 13:09:05 +0530

    Account MS

---
+++ src/test/java/com/c2p/account/AccountserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c2p.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
