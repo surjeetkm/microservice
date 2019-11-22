ad18f6f04b665f103f60d48d7d5031979326a419
surjeetmohanty <surjeetmohanty2019@gmail.com>
2019-11-22 15:41:55 +0530

    Test file

--- src/test/java/com/c2p/account/FizzBuzzTest.java
+++ src/test/java/com/c2p/account/FizzBuzzTest.java
@@ -10,9 +10,9 @@ public class FizzBuzzTest {
 	@Test
     public void multiplicationOfZeroIntegersShouldReturnZero() {
         MyClass tester = new MyClass(); // MyClass is tested
-        assertEquals(12, tester.multiply(10, 2));
-        assertEquals(13, tester.multiply(11, 2));
-        assertEquals(14, tester.multiply(12, 2));
-        assertEquals(15, tester.multiply(13, 2));
+        assertEquals(20, tester.multiply(10, 2));
+        assertEquals(22, tester.multiply(11, 2));
+        assertEquals(24, tester.multiply(12, 2));
+        assertEquals(26, tester.multiply(13, 2));
     }
 }
